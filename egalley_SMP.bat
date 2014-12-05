10741cd76773ad2049f29829edf0a3a8c06fcdfa
nelliemckesson <nellie.mckesson@macmillan.com>
2014-12-05 11:02:46 -0500

    adding egalley bat script

---
+++ egalley_SMP.bat
@@ -0,0 +1,8 @@
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_tmparchive\tmparchive.rb %1
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_htmlmaker\htmlmaker.rb %1
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_filearchive\filearchive.rb %1
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_coverchecker\coverchecker.rb %1
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_imagechecker\imagechecker.rb %1
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_chapterheads\chapterheads.rb %1
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_epubmaker\epubmaker.rb %1
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_cleanup\cleanup.rb %1
