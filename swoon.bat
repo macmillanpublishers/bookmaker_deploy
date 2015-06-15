7d460f708c49af0d733c18b51580de57ee604f0e
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-15 09:55:31 -0400

    adding bat file for swoon html

--- swoon.bat
+++ swoon.bat
@@ -7,12 +7,5 @@ PowerShell -NoProfile -ExecutionPolicy Bypass -Command "S:\resources\bookmaker_s
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\htmlmaker\htmlmaker.rb %1 >> %logfile% 2>&1
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\metadata_preprocessing.rb %1 >> %logfile% 2>&1
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\filearchive\filearchive.rb %1 >> %logfile% 2>&1
-C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\coverchecker\coverchecker.rb %1 >> %logfile% 2>&1
-C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\imagechecker\imagechecker.rb %1 >> %logfile% 2>&1
-C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\stylesheets\stylesheets.rb %1 >> %logfile% 2>&1
-C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\stylesheets_postprocessing.rb %1 >> %logfile% 2>&1
-C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\epubmaker_preprocessing.rb %1 >> %logfile% 2>&1
-C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\epubmaker\epubmaker.rb %1 >> %logfile% 2>&1
-C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\epubmaker_postprocessing.rb %1 >> %logfile% 2>&1
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\cleanup\cleanup.rb %1 >> %logfile% 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "S:\resources\bookmaker_scripts\utilities\mail-alert.ps1 '%1'"
