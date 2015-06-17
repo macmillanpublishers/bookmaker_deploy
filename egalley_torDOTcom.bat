9c5300d44a2bbd2bda83f71d94ab1c74d5748698
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-17 13:47:38 -0400

    adding date and time stamps

--- egalley_torDOTcom.bat
+++ egalley_torDOTcom.bat
@@ -2,6 +2,7 @@
 set logfile="S:\resources\logs\%~n1-stdout-and-err.txt"
 @echo on
 
+@echo %date% %time% >> %logfile% 2>&1
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\tmparchive\tmparchive.rb %1 >> %logfile% 2>&1
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\htmlmaker\htmlmaker.rb %1 >> %logfile% 2>&1
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\metadata_preprocessing.rb %1 >> %logfile% 2>&1
