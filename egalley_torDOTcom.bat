65c597019e6555df0afe3e042683aa63761e08fa
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-17 12:57:33 -0400

    removing ps script

--- egalley_torDOTcom.bat
+++ egalley_torDOTcom.bat
@@ -3,7 +3,6 @@ set logfile="S:\resources\logs\%~n1-stdout-and-err.txt"
 @echo on
 
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\tmparchive\tmparchive.rb %1 >> %logfile% 2>&1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "S:\resources\bookmaker_scripts\WordXML-to-HTML\DocxToXml.ps1 '%1'" >> %logfile% 2>&1
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\htmlmaker\htmlmaker.rb %1 >> %logfile% 2>&1
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\metadata_preprocessing.rb %1 >> %logfile% 2>&1
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\filearchive\filearchive.rb %1 >> %logfile% 2>&1
