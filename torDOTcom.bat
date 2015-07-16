2381bb287728f4645b3700d62b4f4c40ff46d9a1
nelliemckesson <nellie.mckesson@macmillan.com>
2015-07-16 15:09:22 -0400

    fixing imagechecker coverchecker order

--- torDOTcom.bat
+++ torDOTcom.bat
@@ -12,8 +12,8 @@ rem write scriptnames to file for ProcessLogger to rm on success:
 	echo htmlmaker
 	echo metadata_preprocessing
 	echo filearchive
-	echo coverchecker
 	echo imagechecker
+	echo coverchecker
 	echo stylesheets
 	echo stylesheets_postprocessing
 	echo pdfmaker_preprocessing
@@ -36,8 +36,8 @@ C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\tmparchive
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\htmlmaker\htmlmaker.rb %1 >> %logfile% 2>&1 && call :ProcessLogger htmlmaker
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\metadata_preprocessing.rb %1 >> %logfile% 2>&1 && call :ProcessLogger metadata_preprocessing
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\filearchive\filearchive.rb %1 >> %logfile% 2>&1 && call :ProcessLogger filearchive
-C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\coverchecker\coverchecker.rb %1 >> %logfile% 2>&1 && call :ProcessLogger coverchecker
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\imagechecker\imagechecker.rb %1 >> %logfile% 2>&1 && call :ProcessLogger imagechecker
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\coverchecker\coverchecker.rb %1 >> %logfile% 2>&1 && call :ProcessLogger coverchecker
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\stylesheets\stylesheets.rb %1 >> %logfile% 2>&1 && call :ProcessLogger stylesheets
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\stylesheets_postprocessing.rb %1 >> %logfile% 2>&1 && call :ProcessLogger stylesheets_postprocessing
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\pdfmaker_preprocessing.rb %1 >> %logfile% 2>&1 && call :ProcessLogger pdfmaker_preprocessing
