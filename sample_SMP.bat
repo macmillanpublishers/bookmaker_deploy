ed00de8eb0ac632f0a935dd731253b5bca60a871
nelliemckesson <nellie.mckesson@macmillan.com>
2015-08-24 16:06:57 -0400

    pausing execution to allow file to completely copy over

--- sample_SMP.bat
+++ sample_SMP.bat
@@ -28,6 +28,7 @@ rem write scriptnames to file for ProcessLogger to rm on success:
 @echo %date% %time% >> %logfile% 2>&1
 
 start /b PowerShell -NoProfile -ExecutionPolicy Bypass -Command "S:\resources\bookmaker_scripts\utilities\processwatch.ps1 %p_log% '%1'"
+SLEEP 10
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\tmparchive\tmparchive.rb %1 >> %logfile% 2>&1 && call :ProcessLogger tmparchive
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\htmlmaker\htmlmaker.rb %1 >> %logfile% 2>&1 && call :ProcessLogger htmlmaker
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\metadata_preprocessing.rb %1 >> %logfile% 2>&1 && call :ProcessLogger metadata_preprocessing
