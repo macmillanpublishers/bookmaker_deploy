baff90757ef3b6376284ee807a7c89d46dc7831c
nelliemckesson <nellie.mckesson@macmillan.com>
2015-10-29 11:03:56 -0400

    removing coverchecker from swerve pod

--- pod_swerve.bat
+++ pod_swerve.bat
@@ -15,7 +15,6 @@ rem write scriptnames to file for ProcessLogger to rm on success:
 	echo filearchive
 	echo filearchive_postprocessing
 	echo imagechecker
-	echo coverchecker
 	echo stylesheets
 	echo stylesheets_postprocessing
 	echo pdfmaker_preprocessing
@@ -40,7 +39,6 @@ C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\htmlmake
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\filearchive\filearchive.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger filearchive
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\filearchive_postprocessing.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger filearchive_postprocessing
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\imagechecker\imagechecker.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger imagechecker
-C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\coverchecker\coverchecker.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger coverchecker
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\stylesheets\stylesheets.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger stylesheets
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\stylesheets_postprocessing.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger stylesheets_postprocessing
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\pdfmaker_preprocessing.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger pdfmaker_preprocessing
