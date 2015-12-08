a7f56e2db6d32fd9300eaeb0adc4ffddfda9d197
nelliemckesson <nellie.mckesson@macmillan.com>
2015-12-08 15:10:36 -0500

    adding titlepage maker for other ebook projects

--- final_swerve.bat
+++ final_swerve.bat
@@ -11,6 +11,8 @@ rem write scriptnames to file for ProcessLogger to rm on success:
   echo tmparchive
   echo htmlmaker_preprocessing
   echo htmlmaker
+  echo cacert
+  echo titlepage
   echo metadata_preprocessing
   echo htmlmaker_postprocessing
   echo filearchive
@@ -36,6 +38,8 @@ start /b PowerShell -NoProfile -ExecutionPolicy Bypass -Command "S:\resources\bo
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\tmparchive\tmparchive.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger tmparchive
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\htmlmaker_preprocessing.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger htmlmaker_preprocessing
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\htmlmaker\htmlmaker.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger htmlmaker
+SET SSL_CERT_FILE=C:\Ruby193\lib\ruby\site_ruby\1.9.1\rubygems\ssl_certs\cacert.pem >> %logfile% 2>&1 && call :ProcessLogger cacert
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\covermaker\bookmaker_titlepage.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger titlepage
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\metadata_preprocessing.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger metadata_preprocessing
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_addons\htmlmaker_postprocessing.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger htmlmaker_postprocessing
 C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker\core\filearchive\filearchive.rb '%1' >> %logfile% 2>&1 && call :ProcessLogger filearchive
